6c7427abfaa7584ce15349969257901947e09a97
Radius <98346472+Radius112@users.noreply.github.com>
2022-03-31 21:59:41 +0200

    Update OpenLine.h

--- Open/OpenLine/OpenLine.h
+++ Open/OpenLine/OpenLine.h
@@ -1,7 +1,11 @@
 #pragma once
 #include "OpenLineInclude.h"
+
+#define DIV 1024
+#define WIDTH 7
+
 #define BLACK			0
-#define BLUE			1
+#define BLUE		 	1
 #define GREEN			2
 #define CYAN			3
 #define RED				4
@@ -13,129 +17,24 @@
 #define LIGHTGREEN		10
 #define LIGHTCYAN		11
 #define LIGHTRED		12
-#define LIGHTMAGENTA	1a
+#define LIGHTMAGENTA	13
 #define YELLOW			14
 #define WHITE			15
 
+namespace fs = std::filesystem;
 namespace OpenLine {
 	class Function {
 	public:
-		void Link(std::string&& link)
-		{
-			std::string bind = "start ";
-			std::string www = "https://www.";
-			std::string output = bind + www + link;
-			system(output.c_str());
-
-		}
-		void FLink(std::string&& link)
-		{
-			std::string bind = "start ";
-			std::string output = bind + link;
-			system(output.c_str());
-
-		}
-		void FLink(std::string& link)
-		{
-			std::string bind = "start ";
-			std::string output = bind + link;
-			system(output.c_str());
-
-		}
-		void Link(std::string& link)
-		{
-			std::string bind = "start ";
-			std::string www = "https://www.";
-			std::string output = bind + www + link;
-			system(output.c_str());
-
-		}
-		void cowsay(std::string_view text) {
-			std::cout << "___________________________________________________________________\n"
-				<< "                                                                    \n"
-				<< "          < " << text << " > \n"
-				"-------------------------------------------------------------------";
-			std::cout << "\n"
-				"             \\  ^__^\n"
-				"              \\ (oo)\\_______ \n"
-				"                (__) \\       )\\/\\ \n"
-				"                 ||----w |\n"
-				"                 ||     ||\n";
-		}
-		void tux(std::string_view text) {
-			std::cout << "___________________________________________________________________\n"
-				<< "                                                                   \n"
-				<< "           < " << text << " > \n"
-				"------------------------------------------------------------------- \n";
-			std::cout << "                \\ \n"
-				"                 \\ \n"
-				"                   .--.\n"
-				"                  |o_o |\n"
-				"                  |:_/ |\n"
-				"                 //  \\ \\ \n"
-				"                (|     | ) \n"
-				"               /'\\_   _/`\\ \n"
-				"               \\___)=(___/ \n";
-		}
-		void banner()
-		{
-			std::cout << "'########:::::'###::::'########::'####:'##::::'##::'######::::::::'##::'#######::'########::'########:'##::: ##:'##:::::::'####:'##::: ##:'########:\n";
-			std::cout << " ##.... ##:::'## ##::: ##.... ##:. ##:: ##:::: ##:'##... ##::::::'##::'##.... ##: ##.... ##: ##.....:: ###:: ##: ##:::::::. ##:: ###:: ##: ##.....::\n";
-			std::cout << " ##:::: ##::'##:. ##:: ##:::: ##:: ##:: ##:::: ##: ##:::..::::::'##::: ##:::: ##: ##:::: ##: ##::::::: ####: ##: ##:::::::: ##:: ####: ##: ##:::::::\n";
-			std::cout << " ########::'##:::. ##: ##:::: ##:: ##:: ##:::: ##:. ######:::::'##:::: ##:::: ##: ########:: ######::: ## ## ##: ##:::::::: ##:: ## ## ##: ######:::\n";
-			std::cout << " ##.. ##::: #########: ##:::: ##:: ##:: ##:::: ##::..... ##:::'##::::: ##:::: ##: ##.....::: ##...:::: ##. ####: ##:::::::: ##:: ##. ####: ##...::::\n";
-			std::cout << " ##::. ##:: ##.... ##: ##:::: ##:: ##:: ##:::: ##:'##::: ##::'##:::::: ##:::: ##: ##:::::::: ##::::::: ##:. ###: ##:::::::: ##:: ##:. ###: ##:::::::\n";
-			std::cout << " ##:::. ##: ##:::: ##: ########::'####:. #######::. ######::'##:::::::. #######:: ##:::::::: ########: ##::. ##: ########:'####: ##::. ##: ########:\n";
-			std::cout << "..:::::..::..:::::..::........:::....:::.......::::......:::..:::::::::.......:::..:::::::::........::..::::..::........::....::..::::..::........::\n";
-		}
-		void Operation(std::string_view oper)
-		{
-			SetConsoleTextAttribute(h, 9);
-			std::cout << "Output: \n";
-			std::cout << oper << "\n";
-			SetConsoleTextAttribute(h, WHITE);
-		}
-		void Succes(std::string_view oper)
-		{
-			SetConsoleTextAttribute(h, WHITE);
-			std::cout << "Succes: " << oper << "\n";
-			SetConsoleTextAttribute(h, WHITE);
-		}
-		void Error(std::string_view oper)
-		{
-			SetConsoleTextAttribute(h, RED);
-			std::cout << "ERROR: ";
-			std::cout << oper << "\n";
-			SetConsoleTextAttribute(h, WHITE);
-		};
-		void print(auto prints, auto a)
-		{
-			std::cout << prints << "    (" << a << ")" << "\n";
-		}
-		void cmds()
-		{
-			print(".youtube", "Opens youtube link");
-			print(".time", "shows the current time");
-			print(".info", "you just used this lol");
-			print(".notepad", "opens notepad");
-			print(".calc", "opens the calculator");
-			print(".sdown", "shutdowns the computer");
-			print(".exit", "exit out of the application");
-			print(" cls", "clears the console");
-			print(".cowsay", "use it to see what it does");
-			print(".cowsay.tux", "use it to see what it does");
-			print(".open.link", "opens the link you type");
-			print(".open.flink", "does the same as .open.link but you also need to add www and https");
-			print(".c.txt", "Creates a txt file");
-			print(".main.path", "opens the application path");
-			print(".rstart", "restarts the computer");
-			print(".ipc", "shows ip info");
-			print(".chmap", "char map");
-			print(".big.txt", "same as .c.txt but you can type enter");
-			print(".cr.dir", "creates a dir");
-			print(".del.file","deletes a file");
-			print(".dump","dumps all the text");
-			print(".open.path","opens path");
-		}
+		void Link(std::string& link);
+		void Link(std::string&& link);
+		void FLink(std::string& link);
+		void cowsay(std::string_view text); 
+		void tux(std::string_view text) ;
+		void banner();
+	    void Operation(std::string_view oper);		
+		void Succes(std::string_view oper);
+		void Error(std::string_view oper);
+		void print(auto prints, auto a);
+		void cmds();
 	};
 };
